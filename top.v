2a5acf90896b65094d1427c66eb5f3e2905b58d5
Khadija411 <alishba20000@gmail.com>
2023-09-25 18:11:16 +0500

    updated the logic

--- top.v
+++ top.v
@@ -2,10 +2,10 @@ module top(
 	input pixclk,  // 25MHz
 	input reset,
 	output [2:0] TMDSp, TMDSn,
-	// output [5:0] led,
+	output [5:0] led,
 	output TMDSp_clock, TMDSn_clock
 );
-// assign led = {TMDSp,TMDSn};
+assign led = {TMDSp,TMDSn};
 ////////////////////////////////////////////////////////////////////////
 reg [10:0] CounterX=0, CounterY=0;
 reg hSync, vSync, DrawArea;
@@ -14,31 +14,32 @@ always @(posedge pixclk) DrawArea <= (CounterX<1920) && (CounterY<1080);
 always @(posedge pixclk) CounterX <= (CounterX==1919) ? 0 : CounterX+1;
 always @(posedge pixclk) if(CounterX==1919) CounterY <= (CounterY==1079) ? 0 : CounterY+1;
 
-always @(posedge pixclk) hSync <= 1;//(CounterX>=656) && (CounterX<752);
-always @(posedge pixclk) vSync <= 1;//(CounterY>=490) && (CounterY<492);
+always @(posedge pixclk) hSync <= (CounterX>=1920) && (CounterX<2200);
+always @(posedge pixclk) vSync <= (CounterY>=1080) && (CounterY<1125);
 
 ////////////////
-// wire [7:0] W = {8{CounterX[7:0]==CounterY[7:0]}};
-// wire [7:0] A = {8{CounterX[7:5]==3'h2 && CounterY[7:5]==3'h2}};
+wire [7:0] W = {8{CounterX[7:0]==CounterY[7:0]}};
+wire [7:0] A = {8{CounterX[7:5]==3'h2 && CounterY[7:5]==3'h2}};
 reg [7:0] red, green, blue;
-always @(posedge pixclk) red <= 1;//({CounterX[5:0] & {6{CounterY[4:3]==~CounterX[4:3]}}, 2'b00} | W) & ~A;
-always @(posedge pixclk) green <= 1;//(CounterX[7:0] & {8{CounterY[6]}} | W) & ~A;
-always @(posedge pixclk) blue <= 1;//CounterY[7:0] | W | A;
+always @(posedge pixclk) red <= ({CounterX[5:0] & {6{CounterY[4:3]==~CounterX[4:3]}}, 2'b00} | W) & ~A;
+always @(posedge pixclk) green <= (CounterX[7:0] & {8{CounterY[6]}} | W) & ~A;
+always @(posedge pixclk) blue <= CounterY[7:0] | W | A;
 
 ////////////////////////////////////////////////////////////////////////
 wire [9:0] TMDS_red, TMDS_green, TMDS_blue;
 svo_tmds encode_R(.clk(pixclk), .resetn(reset), .din(red  ), .ctrl(2'b00)        , .de(DrawArea), .dout(TMDS_red));
 svo_tmds encode_G(.clk(pixclk), .resetn(reset), .din(green), .ctrl(2'b00)        , .de(DrawArea), .dout(TMDS_green));
-svo_tmds encode_B(.clk(pixclk), .resetn(reset), .din(blue ), .ctrl(2'b00/*{vSync,hSync}*/), .de(DrawArea), .dout(TMDS_blue));
-// TMDS_encoder encode_R(.clk(pixclk), /*.resetn(reset),*/ .VD(red  ), .CD(2'b00)        , .VDE(DrawArea), .TMDS(TMDS_red));
-// TMDS_encoder encode_G(.clk(pixclk), /*.resetn(reset),*/ .VD(green), .CD(2'b00)        , .VDE(DrawArea), .TMDS(TMDS_green));
-// TMDS_encoder encode_B(.clk(pixclk), /*.resetn(reset),*/ .VD(blue ), .CD({vSync,hSync}), .VDE(DrawArea), .TMDS(TMDS_blue));
+svo_tmds encode_B(.clk(pixclk), .resetn(reset), .din(blue ), .ctrl(vSync:hSync), .de(DrawArea), .dout(TMDS_blue));
+
+//TMDS_encoder encode_R(.clk(pixclk), /*.resetn(reset),*/ .VD(red  ), .CD(2'b00)        , .VDE(DrawArea), .TMDS(TMDS_red));
+//TMDS_encoder encode_G(.clk(pixclk), /*.resetn(reset),*/ .VD(green), .CD(2'b00)        , .VDE(DrawArea), .TMDS(TMDS_green));
+//TMDS_encoder encode_B(.clk(pixclk), /*.resetn(reset),*/ .VD(blue ), .CD({vSync,hSync}), .VDE(DrawArea), .TMDS(TMDS_blue)); 
 ////////////////////////////////////////////////////////////////////////
 wire clk_TMDS;// DCM_TMDS_CLKFX;  // 25MHz x 10 = 250MHz
 Gowin_rPLL pll(.clkin(pixclk),.clkout(clk_TMDS));
 // ClockDivider divider (.clk_in(pixclk), .clk_out(clk_TMDS));
-// DCM_SP #(.CLKFX_MULTIPLY(10)) DCM_TMDS_inst(.CLKIN(pixclk), .CLKFX(DCM_TMDS_CLKFX), .RST(1'b0));
-// BUFG BUFG_TMDSp(.I(DCM_TMDS_CLKFX), .O(clk_TMDS));
+//DCM_SP #(.CLKFX_MULTIPLY(10)) DCM_TMDS_inst(.CLKIN(pixclk), .CLKFX(DCM_TMDS_CLKFX), .RST(1'b0));
+//BUFG BUFG_TMDSp(.I(DCM_TMDS_CLKFX), .O(clk_TMDS));
 
 ////////////////////////////////////////////////////////////////////////
 reg [3:0] TMDS_mod10=0;  // modulus 10 counter
@@ -63,11 +64,11 @@ endmodule
 
 ////////////////////////////////////////////////////////////////////////
 // module TMDS_encoder(
-// 	input clk,
-// 	input [7:0] VD,  // video data (red, green or blue)
-// 	input [1:0] CD,  // control data
-// 	input VDE,  // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
-// 	output reg [9:0] TMDS = 0
+//  	input clk,
+//  	input [7:0] VD,  // video data (red, green or blue)
+//  	input [1:0] CD,  // control data
+//  	input VDE,  // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
+//     output reg [9:0] TMDS = 0
 // );
 
 // wire [3:0] Nb1s = VD[0] + VD[1] + VD[2] + VD[3] + VD[4] + VD[5] + VD[6] + VD[7];
@@ -103,22 +104,22 @@ end
 
 endmodule
 
-module ClockDivider (
-    input clk_in,
-    output  clk_out
-);
+// module ClockDivider (
+//     input clk_in,
+//     output  clk_out
+// );
 
-reg [31:0] counter = 0;
+// reg [31:0] counter = 0;
 
-always @(posedge clk_in) begin
-    if (counter == 0) begin
-        clk_out <= ~clk_out;
-        counter <= 200; // Change this value to adjust the frequency division ratio
-    end
-    else begin
-        counter <= counter - 1;
-    end
-end
+// always @(posedge clk_in) begin
+//     if (counter == 0) begin
+//         clk_out <= ~clk_out;
+//         counter <= 200;             // Change this value to adjust the frequency division ratio
+//     end
+//     else begin
+//         counter <= counter - 1;
+//     end
+// end
 
-endmodule
+// endmodule
 ////////////////////////////////////////////////////////////////////////
